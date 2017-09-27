cd7e964b14fddfad2ca06813da11f5db35962d16
qwerty36963 <sparks21012@gmail.com>
2017-09-27 18:15:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function happyHolidays(){return "Happy holidays!"}
 
 function happyHolidaysTo(name){return `Happy holidays, ${name}!`}
 
-function happyHolidaysTo(holiday, name){return `Happy ${holiday}!, ${name}!`}
+
