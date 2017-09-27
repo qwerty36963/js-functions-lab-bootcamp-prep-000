705c761cf62b977a07d5b93b58d3442126d85b6e
qwerty36963 <sparks21012@gmail.com>
2017-09-27 18:13:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function happyHolidays(){return "Happy holidays!"}
-
+var name = you
 function happyHolidaysTo(name){return `Happy holidays, ${name}!`}
-
+var holiday = independence day
 function happyHolidaysTo(holiday, name){return `Happy ${holiday}!, ${name}!`}
